24c530fdcfc79cfa2f5716972e4a3f263071cf40
Walkline Wang <walkline@163.com>
2022-02-27 22:40:44 +0800

    增加 Config 类模块文件

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+The MIT License (MIT)
+Copyright © 2022 Walkline Wang (https://walkline.wang)
+Gitee: https://gitee.com/walkline/micropython-qrcode-cmodule
+"""
+class Config(object):
+	class Pins(object):
+		ADC = 1
+		DC = 4
+		SCL = 6
+		SDA = 7
+		RST = 10
+		UART1_RX = 20
